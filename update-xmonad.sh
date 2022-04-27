2b31453abf195175c12ea248ff31faf2d1680d65
Roland Venesz <roland@venesz.de>
2022-04-27 21:13:28 +0200

    Updated xmonad/xmobar update and build scripts

--- update-xmonad.sh
+++ update-xmonad.sh
@@ -3,6 +3,18 @@
 cd
 cabal update
 
+link () {
+  local bin=$(jq -r '.["install-plan"][] | select(.["component-name"] == "exe:'$1'")["bin-file"]' dist-newstyle/cache/plan.json)
+  if [[ -f $bin ]]; then
+    local bname=$(basename $bin)
+    local dest=~/.local/bin/$bname
+    if [[ ! -f $dest || $(stat -L -c %d:%i $bin) != $(stat -L -c %d:%i $dest) ]]; then
+      echo === Linking $bname
+      ln -f $bin ~/.local/bin/
+    fi
+  fi
+}
+
 for i in ~/.{xmonad,xmobar}/*-git; do
   echo "=== Pulling $(basename $i)"
   cd $i
@@ -11,8 +23,10 @@ done
 
 cd ~/.xmonad
 echo "=== Building xmonad"
-cabal build xmonad xmonad-contrib
+cabal build xmonad xmonad-contrib --upgrade-dependencies
+link xmonad
 
 echo "=== Building xmobar"
 cd ~/.xmobar
-cabal build xmobar
+cabal build xmobar --upgrade-dependencies
+link xmobar
