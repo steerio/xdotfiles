cbb42c3f05616ee57c2445d5354f2174cc6e0fdc
Roland Venesz <roland@venesz.de>
2022-07-27 02:23:08 +0200

    Overlap layout (kind of WIP)

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE FlexibleInstances, MultiParamTypeClasses, LambdaCase #-}
 
 import XMonad
 import XMonad.Hooks.InsertPosition
@@ -17,7 +17,7 @@ import Graphics.X11.ExtraTypes.XF86
 main = do
   bar <- spawnPipe "xmobar ~/.xmobar/xmobar.hs"
   xmonad $ ewmh . ewmhFullscreen . docks $
-           def { layoutHook = avoidStruts $ smartBorders $ gaps tall ||| Full
+           def { layoutHook = avoidStruts $ tall ||| overlap ||| noBorders Full
                , terminal = "urxvt"
                , focusFollowsMouse = False
                , manageHook = insertPosition Below Newer
@@ -26,9 +26,13 @@ main = do
                , modMask = mod
                , normalBorderColor = "#505050" } `additionalKeys` keys
     where
-      tall = ResizableTall 1 (4/120) (17/25) []
-      border = Border 4 4 4 4
-      gaps = spacingRaw True border True border True
+      tall = let b = Border 4 4 4 4 in
+        smartBorders $ spacingRaw True b True b True $
+          ResizableTall 1 (4/120) (17/25) []
+      overlap =
+        spacingRaw False (Border 4 4 8 8) True (Border 4 4 0 0) True $
+          Overlap 1 (Take 252) (Take 400) 8
+
       pp = def { ppCurrent = xmobarColor "white" ""
                , ppTitle = xmobarColor "#b8bb26" "" . shorten 120
                , ppSep = " <fc=#7c6f64>|</fc> " }
@@ -43,12 +47,11 @@ main = do
         , ((mod, xK_bracketright), spawn "xdotool mousemove_relative 6 0; import ~/screenshot-`date '+%Y-%m-%d-%H%M%S'`.png")
         , ((mod, xK_b), spawn "chromium")
         , ((mod, xK_Return), windows smartSwap)
-        , ((mod, xK_Up),    sendMessage MirrorExpand)
-        , ((mod, xK_Down),  sendMessage MirrorShrink)
-        , ((msh, xK_Left),  withFocused $ keysMoveWindow (-1, 0))
-        , ((msh, xK_Right), withFocused $ keysMoveWindow (1, 0))
-        , ((msh, xK_Up),    withFocused $ keysMoveWindow (0, -1))
-        , ((msh, xK_Down),  withFocused $ keysMoveWindow (0, 1))
+        , ((mod, xK_Up),    sendMessage $ JumpToLayout "Full")
+        , ((mod, xK_Down),  sendMessage $ JumpToLayout "Spacing ResizableTall")
+        , ((mod, xK_Left),  sendMessage $ JumpToLayout "Spacing Overlap")
+        , ((msh, xK_Up),    sendMessage MirrorExpand)
+        , ((msh, xK_Down),  sendMessage MirrorShrink)
         , ((0, xF86XK_MonBrightnessUp), spawn "xbacklight -inc 10")
         , ((0, xF86XK_MonBrightnessDown), spawn "xbacklight -dec 10") ]
         ++
@@ -57,7 +60,60 @@ main = do
       mod = mod4Mask
       msh = mod .|. shiftMask
 
+-- Smart Swap
+
 smartSwap :: StackSet i l a s sd -> StackSet i l a s sd
 smartSwap = modify' $ \case
   Stack t [] (r:rs) -> Stack r [] (t:rs)
   Stack t ls rs -> Stack t [] (xs ++ x : rs) where (x:xs) = reverse ls
+
+-- Overlap layout (inspired by Slack)
+
+data Margin = Take !Dimension | Keep !Dimension | Ratio !Rational | Bigger !Margin !Margin | Edge
+  deriving (Show, Read)
+
+withMargin :: Dimension -> Margin -> Dimension
+withMargin dim = \case
+  (Keep n)     -> n
+  (Take n)     -> dim - n
+  (Ratio r)    -> floor (r * fromIntegral dim)
+  (Bigger a b) -> max (withMargin dim a) (withMargin dim b)
+  Edge         -> dim
+
+data Overlap a = Overlap { overlapNMaster :: !Int
+                         , overlapLeft :: !Margin
+                         , overlapRight :: !Margin
+                         , overlapGap :: !Dimension }
+  deriving (Show, Read)
+
+instance LayoutClass Overlap a where
+  pureLayout (Overlap nmaster left right gap) r s = shuffle $ zip ws rs
+    where
+      ws = integrate s
+      rs = overlap left right gap r nmaster (length ws)
+      shuffle xs = if length (up s) < nmaster then xs else drop nmaster xs ++ take nmaster xs
+
+  pureMessage (Overlap n l r g) msg = inc <$> fromMessage msg
+    where
+      inc (IncMasterN d) = Overlap (max 0 n+d) l r g
+
+  description _ = "Overlap"
+
+overlap
+    :: Margin
+    -> Margin
+    -> Dimension
+    -> Rectangle
+    -> Int
+    -> Int
+    -> [Rectangle]
+overlap left right gap r@(Rectangle x y w h) nmaster n = if n <= nmaster || nmaster == 0
+    then splitVertically n r
+    else splitVertically nmaster r1 ++ splitVertically (n-nmaster) r2
+  where
+    r1 = let w' = calc left in
+           Rectangle (fromIntegral (gap + w - w')) (y + fromIntegral gap) w' h'
+    r2 = Rectangle x y (calc right) h'
+    calc = let w' = w - gap
+           in max 1 . min w' . withMargin w'
+    h' = h - gap
